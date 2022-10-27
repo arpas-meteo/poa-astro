1fafa79a27dfba355da68f9538daf1965556339e
Andrea Lai <alai.arpas@gmail.com>
2022-10-27 11:46:59 +0200

    esempio charts

--- src/components/Poareacharts.tsx
+++ src/components/Poareacharts.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import { useEffect, useState } from 'react';
 import { Fragment } from 'react';
 import { IStazione } from '../tipi/stazione';
+import { Example } from './Example'
 
 
 export interface Props {
   stazio: IStazione;
 }
-
 
 export default function Poareacharts(props: Props) {
   const stazio = props.stazio;
@@ -20,16 +20,27 @@ export default function Poareacharts(props: Props) {
   let fine = stazio.DATA_FINE
   if (stazio.DATA_FINE === "") fine = "è attiva"
 
+  const [data_inizio, setInizio] = useState("2022-01");
+  const [data_fine, setInisetFine] = useState("2022-12");
 
+  const onChangeHandlerInizio = event => {
+    setInizio(event.target.value);
+    console.log(event.target.value)
+  };
 
+  const onChangeHandlerFine = event => {
+    setInisetFine(event.target.value);
+    console.log(event.target.value)
+  };
 
   const interroga = []
   for (let i = 0; i < valori.length; i++) {
     const v = valori[i];
-    const url = `https://poa.alai.one/poa/quest/${stazio.COD_STAZ}/${v}?inizio=2022&fine=2023 `;
+    const url = `https://poa.alai.one/poa/quest/${stazio.COD_STAZ}/${v}?inizio=2022&fine=2023`;
     const obj = { 'key': v, 'url': url }
     interroga.push(obj)
   }
+
 
   /* const local_route = "http://192.168.24.72:5009/poa/anagrafica"
   
@@ -85,30 +96,37 @@ export default function Poareacharts(props: Props) {
             <span className="text-black font-bold"> {stazio.TIPO_STAZ}</span>
           </h3>
         </div>
-      </div>
-      <br />
+      </div><br />
       <div>
         <div>
           <h3>Inizio misure: <span className="text-black font-semibold"> {stazio.DATA_INIZIO}</span> </h3>
           <h3>Fine misure: <span className="text-black font-semibold"> {fine}</span> </h3>
         </div>
-      </div>
-      <br />
+      </div><br />
       <div>
         <h4>Le date</h4>
         <label htmlFor="id_inizio">Scegli data iniziale:</label>
-        <input type="datetime" name="id_inizio" id="id_inizio" />
+        <input type="datetime" name="id_inizio" id="id_inizio"
+          onChange={onChangeHandlerInizio}
+          value={data_inizio} />
         <label htmlFor="id_fine">Scegli data fine:</label>
-        <input type="datetime" name="id_fine" id="id_fine" />
-      </div>
+        <input type="datetime" name="id_fine" id="id_fine"
+          onChange={onChangeHandlerFine}
+          value={data_fine} />
+      </div><br />
       <ul>
         Valori presenti:
         {interroga.map((v) => (
           <li key={v.key}>
-            <a href={v.url} >{v.key}</a>
+            <button className="text text-xl bg-blue-100 m-2 border-spacing-1 border-lime-900">
+              <a href={`https://poa.alai.one/poa/quest/${stazio.COD_STAZ}/${v.key}?inizio=${data_inizio}&fine=${data_fine}`} >{v.key}</a>
+            </button>
+            <div>{`https://poa.alai.one/poa/quest/${stazio.COD_STAZ}/${v.key}?inizio=${data_inizio}&fine=${data_fine}`}</div>
           </li>
         ))}
       </ul>
+      <br />
+      <Example></Example>
     </div>
     // <ul>
     //   {poa_ana.map((ana) => (
