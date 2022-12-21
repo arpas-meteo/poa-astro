fdeebe5649202233bdb090c41c0597f0c4b4c2da
Andrea Lai <alai.arpas@gmail.com>
2022-12-21 11:02:56 +0100

    download anagrafica

--- download_anagrafica.js
+++ download_anagrafica.js
@@ -5,7 +5,7 @@ const http_url = "http://15.161.134.66:8000/poa/anagrafica";
 fetch(http_url)
   .then((response) => response.json())
   .then((data) => {
-    const nome_file = "./src/data/anagrafica.json";
+    const nome_file = "./src/tipi/anagrafica.json";
     const string_data = JSON.stringify(data, null, 2);
     fs.writeFileSync(nome_file, string_data);
 
