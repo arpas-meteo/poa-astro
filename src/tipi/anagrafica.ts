9d1073e55ee574ead77123b963eb229c17878c31
Andrea Lai <alai.arpas@gmail.com>
2022-12-21 10:55:37 +0100

    anagrafica json

---
+++ src/tipi/anagrafica.ts
@@ -0,0 +1,13 @@
+
+import data from './anagrafica.json' assert {type: 'json'};
+
+/* export const get_anagrafica = function (){
+    fetch(nome_file)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(nome_file);
+        return data;
+    });
+}
+ */
+export const anagrafica = data;
